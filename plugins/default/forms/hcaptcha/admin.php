40642971568d1bceb4058a04e285226b0a078e5b
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-06-11 19:26:35 +0800

    Update admin.php

--- plugins/default/forms/hcaptcha/admin.php
+++ plugins/default/forms/hcaptcha/admin.php
@@ -1,12 +1,20 @@
-    <p><?php echo ossn_print('hcaptcha:com:note');?></p>
+<p><?php echo ossn_print('hcaptcha:com:note');?></p>
  <div>
  	<label><?php echo ossn_print('hcaptcha:com:site_key');?></label>
-    <input type="text" name="hcaptcha_site_key" value="<?php echo $params['hcaptcha']->hcaptcha_site_key;?>" />
+    <?php if(is_object($params['hcaptcha'])) { ?>
+        <input type="text" name="hcaptcha_site_key" value="<?php echo $params['hcaptcha']->hcaptcha_site_key;?>" />
+    <?php } else { ?>
+        <input type="text" name="hcaptcha_site_key" value="" />
+    <?php } ?>
  </div>
  <div>
    <label><?php echo ossn_print('hcaptcha:com:secret_key');?></label>
-   <input type="text" name="hcaptcha_secret_key" value="<?php echo $params['hcaptcha']->hcaptcha_secret_key;?>" />
- </div>
- <div>
- 	<input type="submit" class="btn btn-success"/>
- </div>
+   <?php if(is_object($params['hcaptcha'])) { ?>
+       <input type="text" name="hcaptcha_secret_key" value="<?php echo $params['hcaptcha']-> hcaptchasecretkey;?>" />
+   <?php } else { ?>
+       <input type="text" name=" hcaptchasecretkey "value="" />
+   <? php}?>
+ </div> 
+<div> 
+    	<input type = "submit"class ="btn btn-success"/> 
+</ div>
