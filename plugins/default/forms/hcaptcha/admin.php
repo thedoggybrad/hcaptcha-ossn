9ccd5d5dc84caa5a197967fdb2fa94fd2909f06e
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-06-11 20:11:43 +0800

    Finally Fixed Code
    Thanks to ChatGPT

--- plugins/default/forms/hcaptcha/admin.php
+++ plugins/default/forms/hcaptcha/admin.php
@@ -1,20 +1,31 @@
-<p><?php echo ossn_print('hcaptcha:com:note');?></p>
- <div>
- 	<label><?php echo ossn_print('hcaptcha:com:site_key');?></label>
-    <?php if(is_object($params['hcaptcha'])) { ?>
-        <input type="text" name="hcaptcha_site_key" value="<?php echo $params['hcaptcha']->hcaptcha_site_key;?>" />
-    <?php } else { ?>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<?php
+echo "<p>" . ossn_print('hcaptcha:com:note') . "</p>";
+?>
+
+<div>
+    <label><?php echo ossn_print('hcaptcha:com:site_key'); ?></label>
+    <?php if (isset($params['hcaptcha']) && isset($params['hcaptcha']->hcaptcha_site_key)): ?>
+        <input type="text" name="hcaptcha_site_key" value="<?php echo $params['hcaptcha']->hcaptcha_site_key; ?>" />
+    <?php else: ?>
         <input type="text" name="hcaptcha_site_key" value="" />
-    <?php } ?>
- </div>
- <div>
-   <label><?php echo ossn_print('hcaptcha:com:secret_key');?></label>
-   <?php if(is_object($params['hcaptcha'])) { ?>
-       <input type="text" name="hcaptcha_secret_key" value="<?php echo $params['hcaptcha']-> hcaptchasecretkey;?>" />
-   <?php } else { ?>
-       <input type="text" name=" hcaptchasecretkey "value="" />
-   <? php}?>
- </div> 
-<div> 
-    	<input type = "submit"class ="btn btn-success"/> 
-</ div>
+    <?php endif; ?>
+</div>
+
+<div>
+    <label><?php echo ossn_print('hcaptcha:com:secret_key'); ?></label>
+    <?php if (isset($params['hcaptcha']) && isset($params['hcaptcha']->hcaptcha_secret_key)): ?>
+        <input type="text" name="hcaptcha_secret_key" value="<?php echo $params['hcaptcha']->hcaptcha_secret_key; ?>" />
+    <?php else: ?>
+        <input type="text" name="hcaptcha_secret_key" value="" />
+    <?php endif; ?>
+</div>
+
+<div>
+    <input type="submit" class="btn btn-success" />
+</div>
