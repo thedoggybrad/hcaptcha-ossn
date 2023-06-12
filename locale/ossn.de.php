3e2e51ee3b585e91888b0e3f7ceaf2dc32a916b1
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-06-12 10:09:20 +0800

    Update ossn.de.php

--- locale/ossn.de.php
+++ locale/ossn.de.php
@@ -6,7 +6,7 @@ $de = array(
     'hcaptcha:error' => 'Sie haben ein ungültiges hcaptcha eingegeben',
     'hcaptcha:com:site_key' => 'hcaptcha SITE_KEY',
     'hcaptcha:com:secret_key' => 'hcaptcha ACCOUNT_SECRET_KEY',
-    'hcaptcha:com:note' => 'Wir benötigen die API-Schlüssel. Gehen Sie zu <a href="https://dashboard.hcaptcha.com/">https://dashboard.hcaptcha.com/</a>. Um  Zugriff auf diese Seite zu erhalten, müssen Sie auf Ihr hCaptcha-Konto zugreifen. Registrieren Sie den Namen Ihrer Website, auf der dieses hCAPTCHA     verwendet wird. Danach fügen Sie sowohl ACCOUNT_SECRET_KEY als auch SITE_KEY in jedes Feld hier ein.',
+    'hcaptcha:com:note' => 'Wir benötigen die API-Schlüssel. Gehen Sie zu <a href="https://dashboard.hcaptcha.com/">https://dashboard.hcaptcha.com/</a>. Um  Zugriff auf diese Seite zu erhalten, müssen Sie auf Ihr hCaptcha-Konto zugreifen. Registrieren Sie den Namen Ihrer Website, auf der dieses hCAPTCHA verwendet wird. Danach fügen Sie sowohl ACCOUNT_SECRET_KEY als auch SITE_KEY in jedes Feld hier ein.',
     'hcaptcha:site_key:empty' => 'Das SITE_KEY-Feld ist leer.',
     'hcaptcha:secret_key:empty' => 'Das SECRET_KEY-Feld ist leer.',
     'hcaptcha:saved' => 'API-Schlüssel erfolgreich gespeichert!',
