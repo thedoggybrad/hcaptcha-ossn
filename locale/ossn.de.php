e564c8c404cc5b759b63f335b74c08d98d7d00c4
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-06-12 10:01:35 +0800

    Create ossn.de.php

---
+++ locale/ossn.de.php
@@ -0,0 +1,15 @@
+<?php
+
+$de = array(
+    'hcaptcha' => 'hCAPTCHA von Intuition Machines',
+    'hcaptcha:text' => 'Bitte füllen Sie das hcaptcha aus',
+    'hcaptcha:error' => 'Sie haben ein ungültiges hcaptcha eingegeben',
+    'hcaptcha:com:site_key' => 'hcaptcha SITE_KEY',
+    'hcaptcha:com:secret_key' => 'hcaptcha ACCOUNT_SECRET_KEY',
+    'hcaptcha:com:note' => 'Wir benötigen die API-Schlüssel. Gehen Sie zu <a href="https://dashboard.hcaptcha.com/">https://dashboard.hcaptcha.com/</a>. Um  Zugriff auf diese Seite zu erhalten, müssen Sie auf Ihr hCaptcha-Konto zugreifen. Registrieren Sie den Namen Ihrer Website, auf der dieses hCAPTCHA     verwendet wird. Danach fügen Sie sowohl ACCOUNT_SECRET_KEY als auch SITE_KEY in jedes Feld hier ein.',
+    'hcaptcha:site_key:empty' => 'Das SITE_KEY-Feld ist leer.',
+    'hcaptcha:secret_key:empty' => 'Das SECRET_KEY-Feld ist leer.',
+    'hcaptcha:saved' => 'API-Schlüssel erfolgreich gespeichert!',
+    'hcaptcha:save:error' => 'Die API-Schlüssel können nicht gespeichert werden.',
+);
+ossn_register_languages('de', $de);
